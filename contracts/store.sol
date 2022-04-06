4fd8f90d0fd6b2b9fe0bf45c19a70009b0940042
Michelle David <rachaelholy@gmail.com>
2022-04-07 00:39:23 +0100

    first commit

---
+++ contracts/store.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// declare solidity version 
+
+pragma solidity ^0.8.0;
+import "hardhat/console.sol";
+
+ 
+
+contract sayHi {
+uint256 totalMsg;
+
+event NewMessage(address indexed from, uint256 timestamp, string message );
+
+struct SendMsg {
+    address sender;  // address of the sender
+    string message;  // the message the sender sent
+    uint256  timestamp; // the timestamp when the sender sent the message
+
+}
+// declare a variable that let me store my struct 
+SendMsg[] sendMsgs; 
+
+constructor() {
+    console.log("Hey you just called me!");
+}
+
+function sendmsg(string memory _message) public {
+    totalMsg += 1;
+    console.log("%s messages sent %s", msg.sender, _message);
+
+    sendMsgs.push(SendMsg(msg.sender, _message, block.timestamp));
+
+    emit NewMessage(msg.sender, block.timestamp, _message); 
+}
+
+// this function will return the struct array and all messages 
+function getAllMessages() public view returns(SendMsg[] memory) {
+   return sendMsgs; 
+}
+
+// to print the value
+function getTotalMessage() public view returns(uint256) {
+    console.log("there is %d total message", totalMsg);
+    return totalMsg;
+}
+
+}
---
+++ scripts/run.js
@@ -0,0 +1,39 @@
+const main = async () => {
+     //  grab the wallet address of contract owner and a random wallet address
+    const [ owner, randomPerson ] = await hre.ethers.getSigners();
+    
+    // this will compile the contract and generate the necessary files needed to work with the contract under the artifacts
+const msgContractFactory = await hre.ethers.getContractFactory("sayHi");
+
+// Hardhat to create a local Ethereum network
+const msgContract = await msgContractFactory.deploy();
+
+// wait until the contract officially deployed to the blockchain
+await msgContract.deployed(); 
+
+console.log("contract deployed to..", msgContract.address);
+console.log("contract deployed by..", owner.address);
+
+let msgCount;
+msgCount = await msgContract.getTotalMessage();
+
+let sendmsgTxn = await msgContract.sendmsg(); 
+await sendmsgTxn.wait();
+msgCount = await msgContract.getTotalMessage();
+
+let getAllMessagesTxn = msgContract.getAllMessages();
+await getAllMessagesTxn.wait();
+msgCount = await msgContract.getTotalMessage();
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0); // exit node process without error
+
+    } catch (error) {
+        process.exit(1); // exit node process while indicating 'Uncaught Fatal Exception' error
+    console.log(error); 
+    }
+};
+runMain();
